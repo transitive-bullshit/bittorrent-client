96227a0462c60148e2066c0522e7735a55734b61
Feross Aboukhadijeh <feross@feross.org>
2014-05-11 19:44:16 -0700

    Test supported torrentInfo types

--- test/basic.js
+++ test/basic.js
@@ -7,6 +7,44 @@ var fs = require('fs')
 
 var leaves = fs.readFileSync(__dirname + '/torrents/leaves.torrent')
 var leavesTorrent = parseTorrent(leaves)
+var leavesInfoHash = 'd2474e86c95b19b8bcfdb92bc12c9d44667cfa36'
+
+test('Test supported torrentInfo types', function (t) {
+  t.plan(4)
+
+  function verify (client, torrent) {
+    t.equal(torrent.infoHash, leavesInfoHash)
+    client.destroy()
+  }
+
+  // info hash as a hex string
+  var client1 = BitTorrentClient()
+    .add(leavesInfoHash)
+    .once('addTorrent', function (torrent) {
+      verify(client1, torrent)
+    })
+
+  // info hash as a buffer
+  var client2 = BitTorrentClient()
+    .add(new Buffer(leavesInfoHash, 'hex'))
+    .once('addTorrent', function (torrent) {
+      verify(client2, torrent)
+    })
+
+  // magnet uri (as a utf8 string)
+  var client3 = BitTorrentClient()
+    .add('magnet:?xt=urn:btih:' + leavesInfoHash)
+    .once('addTorrent', function (torrent) {
+      verify(client3, torrent)
+    })
+
+  // .torrent file (as a Buffer)
+  var client4 = BitTorrentClient()
+    .add(leaves)
+    .once('addTorrent', function (torrent) {
+      verify(client4, torrent)
+    })
+})
 
 // TODO: replace this with a test that can run offline
 test('Download "Pride and Prejudice" by Jane Austen', function (t) {
