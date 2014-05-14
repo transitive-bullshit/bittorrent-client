876219494c54dddc91c59056b3ac118a297be1ea
fisch0920 <fisch0920@gmail.com>
2014-05-14 09:35:12 -0400

    LARGE CHANGE; converted Storage to async interface, added readable stream, added file/range selection, and imported the torrent/swarm/request management logic from torrent_stream

--- test/transfer.js
+++ test/transfer.js
@@ -14,8 +14,8 @@ test('external download and transfer between local torrents', function (t) {
 
   // clientA will download the torrent from external peers normally, whereas
   // the isolated clientB will download from its sole peer clientA.
-  var clientA = new BitTorrentClient() // enable external peer discovery
-  var clientB = new BitTorrentClient({ dht: false, trackers: false }) // disable external peer discovery
+  var clientA = new BitTorrentClient({ chokeTimeout: Infinity }) // enable external peer discovery
+  var clientB = new BitTorrentClient({ dht: false, trackers: false, chokeTimeout: Infinity }) // disable external peer discovery
 
   // clientA starts with metadata from torrent file
   clientA.add(leaves)
