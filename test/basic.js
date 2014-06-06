ef7828229274b267418240949e7a9340f2c5f22b
Feross Aboukhadijeh <feross@feross.org>
2014-06-05 20:49:47 -0700

    disable dht+trackers for basic test

--- test/basic.js
+++ test/basic.js
@@ -6,39 +6,38 @@ var fs = require('fs')
 
 var leaves = fs.readFileSync(__dirname + '/torrents/leaves.torrent')
 var leavesTorrent = parseTorrent(leaves)
-var leavesInfoHash = 'd2474e86c95b19b8bcfdb92bc12c9d44667cfa36'
 
 test('Test supported torrentInfo types', function (t) {
   t.plan(4)
 
   function verify (client, torrent) {
-    t.equal(torrent.infoHash, leavesInfoHash)
+    t.equal(torrent.infoHash, leavesTorrent.infoHash)
     client.destroy()
   }
 
   // info hash as a hex string
-  var client1 = BitTorrentClient()
-    .add(leavesInfoHash)
+  var client1 = BitTorrentClient({ dht: false, trackers: false })
+    .add(leavesTorrent.infoHash)
     .once('addTorrent', function (torrent) {
       verify(client1, torrent)
     })
 
   // info hash as a buffer
-  var client2 = BitTorrentClient()
-    .add(new Buffer(leavesInfoHash, 'hex'))
+  var client2 = BitTorrentClient({ dht: false, trackers: false })
+    .add(new Buffer(leavesTorrent.infoHash, 'hex'))
     .once('addTorrent', function (torrent) {
       verify(client2, torrent)
     })
 
   // magnet uri (as a utf8 string)
-  var client3 = BitTorrentClient()
-    .add('magnet:?xt=urn:btih:' + leavesInfoHash)
+  var client3 = BitTorrentClient({ dht: false, trackers: false })
+    .add('magnet:?xt=urn:btih:' + leavesTorrent.infoHash)
     .once('addTorrent', function (torrent) {
       verify(client3, torrent)
     })
 
   // .torrent file (as a Buffer)
-  var client4 = BitTorrentClient()
+  var client4 = BitTorrentClient({ dht: false, trackers: false })
     .add(leaves)
     .once('addTorrent', function (torrent) {
       verify(client4, torrent)
